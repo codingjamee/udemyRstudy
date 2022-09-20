4661cd7546cc89b3a3dfaa519e4b125db8ddc0a7
jenner <codingjamee@gmail.com>
2022-09-20 22:17:17 +0900

    Input 컴포넌트까지 만들어보기

---
+++ cart/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./conponents/Layout/Header";
+import Meals from "./conponents/Meals/Meals";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </div>
+  );
+}
+
+export default App;
