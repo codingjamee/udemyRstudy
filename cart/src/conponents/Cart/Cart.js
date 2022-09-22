db7162f9a181e02c41714ae6b1450a122cb8d934
jenner <codingjamee@gmail.com>
2022-09-22 22:24:28 +0900

    item추가하여 장바구니에서 버튼으로 수량조절하기

--- cart/src/conponents/Cart/Cart.js
+++ cart/src/conponents/Cart/Cart.js
@@ -1,17 +1,38 @@
+import { useContext } from "react";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 //모든 장바구니 항목 렌더링
 //총수량 장바구니 나가기, 주문하기 버튼 필요
 
 const Cart = props => {
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+  const cartItemRemoveHandler = id => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = item => {
+    cartCtx.addItem(item);
+  };
+
   const onClickOpen = () => {
     props.onClickOpen(false);
   };
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map(item => (
-        <li>{item.name}</li>
+      {cartCtx.items.map(item => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
@@ -19,15 +40,15 @@ const Cart = props => {
   return (
     <Modal onClickOpen={props.onClickOpen}>
       {cartItems}
-      <div>
-        <span className={classes.total}>Total Amount</span>
-        <span>35.62</span>
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={onClickOpen}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
--- cart/src/conponents/Layout/HeaderCartButton.js
+++ cart/src/conponents/Layout/HeaderCartButton.js
@@ -11,13 +11,16 @@ const HeaderCartButton = props => {
   const onClickOpen = () => {
     props.onClickOpen(true);
   };
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
   return (
     <button className={classes.button} onClick={onClickOpen}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>{cartCtx.items}</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
