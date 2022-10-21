04790dad14847b7caa1ddd655220dce5fd684b6e
jenner <codingjamee@gmail.com>
2022-10-21 23:58:22 +0900

    리액트 라우팅

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,7 +1,20 @@
+import { Route } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
