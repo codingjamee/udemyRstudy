a0b49e3f5f839df27166bceb566cc4345f521350
jamee <codingjamee@gmail.com>
2022-10-24 17:31:43 +0900

    리액트 라우터 exact와 switch 배우기

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,19 +1,28 @@
-import { Route } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import MainHeader from "./components/MainHeader";
 import Products from "./pages/Products";
 import Welcome from "./pages/Welcome";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
