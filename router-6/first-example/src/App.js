f1ac5e26a563bd9b143fc76295efcf45dd9f97b5
jamee <codingjamee@gmail.com>
2022-11-08 13:22:55 +0900

    react-router 6.4 main features lecture

---
+++ router-6/first-example/src/App.js
@@ -0,0 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import MainHeader from "./components/MainHeader";
+
+function App() {
+  return (
+    <div>
+      <MainHeader />
+      <main>
+        <Routes>
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:productId" element={<ProductDetail />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
+// our-domain.com/welcome => Welcome Component
+// our-domain.com/products => Products Component
+// our-domain.com/product-detail/a-book
---
+++ router-6/second-example/src/pages/Products.js
@@ -0,0 +1,24 @@
+import { Link, useNavigate } from "react-router-dom";
+
+const Products = () => {
+  const navigate = useNavigate();
+  navigate("/welcome", { replace: true });
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>
+          <Link to="/products/p1">A Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">A Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">An Online Course</Link>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
