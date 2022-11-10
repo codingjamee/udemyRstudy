2d37810d8323c69eb3aecb02bd5319e43477a78a
jamee <codingjamee@gmail.com>
2022-11-10 16:11:08 +0900

    commit changes

--- react-router-practice/build/static/js/6.e7e1fe11.chunk.js
+++ react-router-practice/build/static/js/6.e7e1fe11.chunk.js
@@ -1,2 +1,2 @@
 (this["webpackJsonpreact-complete-guide"]=this["webpackJsonpreact-complete-guide"]||[]).push([[6],{53:function(e,t,c){"use strict";c.r(t);var n=c(1);t.default=function(){return Object(n.jsx)("div",{className:"centered",children:Object(n.jsx)("p",{children:"Page not found!"})})}}}]);
-//# sourceMappingURL=6.110d6d15.chunk.js.map
+//# sourceMappingURL=6.e7e1fe11.chunk.js.map
--- react-router-practice/src/components/quotes/QuoteForm.js
+++ react-router-practice/src/components/quotes/QuoteForm.js
@@ -1,13 +1,10 @@
-import { Fragment, useRef, useState } from "react";
-import { Prompt } from "react-router-dom";
+import { Fragment, useRef } from "react";
 
-import Card from "../ui/Card";
-import LoadingSpinner from "../ui/LoadingSpinner";
+import Card from "../UI/Card";
+import LoadingSpinner from "../UI/LoadingSpinner";
 import classes from "./QuoteForm.module.css";
 
 const QuoteForm = (props) => {
-  const [isEntering, setIsEntering] = useState(false);
-
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
@@ -22,28 +19,10 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
-  const finishEnteringHandler = () => {
-    setIsEntering(false);
-  };
-
-  const formFocusedHandler = () => {
-    setIsEntering(true);
-  };
-
   return (
     <Fragment>
-      <Prompt
-        when={isEntering}
-        message={(location) =>
-          "Are you sure you want to leave? All your entered data will be lost!"
-        }
-      />
       <Card>
-        <form
-          onFocus={formFocusedHandler}
-          className={classes.form}
-          onSubmit={submitFormHandler}
-        >
+        <form className={classes.form} onSubmit={submitFormHandler}>
           {props.isLoading && (
             <div className={classes.loading}>
               <LoadingSpinner />
@@ -51,15 +30,15 @@ const QuoteForm = (props) => {
           )}
 
           <div className={classes.control}>
-            <label htmlFor="author">Author</label>
+            <label htmlFor="author">글쓴이</label>
             <input type="text" id="author" ref={authorInputRef} />
           </div>
           <div className={classes.control}>
-            <label htmlFor="text">Text</label>
+            <label htmlFor="text">내용</label>
             <textarea id="text" rows="5" ref={textInputRef}></textarea>
           </div>
           <div className={classes.actions}>
-            <button className="btn">Add Quote</button>
+            <button className="btn">글추가하기</button>
           </div>
         </form>
       </Card>
