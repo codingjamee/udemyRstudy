a5b44c7756264a2941b3d049d905bcefe65a36b0
jamee <codingjamee@gmail.com>
2022-12-05 23:39:59 +0900

    fetch를 사용해서 firebase에 데이터 전송하고, 가져오기

--- 28-react-hook/src/components/Ingredients/IngredientForm.js
+++ 28-react-hook/src/components/Ingredients/IngredientForm.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from "../UI/Card";
+import "./IngredientForm.css";
 
-const IngredientForm = React.memo(props => {
-  const submitHandler = event => {
+const IngredientForm = React.memo((props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+
+  const submitHandler = (event) => {
     event.preventDefault();
-    // ...
+    props.onAddItem({ title: enteredTitle, amount: enteredAmount });
   };
 
   return (
@@ -15,11 +18,25 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input
+              type="text"
+              id="title"
+              value={enteredTitle}
+              onChange={(event) => {
+                setEnteredTitle(event.target.value);
+              }}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input
+              type="number"
+              id="amount"
+              value={enteredAmount}
+              onChange={(event) => {
+                setEnteredAmount(event.target.value);
+              }}
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
--- 28-react-hook/src/components/Ingredients/Ingredients.js
+++ 28-react-hook/src/components/Ingredients/Ingredients.js
@@ -1,19 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
 
-function Ingredients() {
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://react-http-55f5b-default-rtdb.firebaseio.com/ingredients.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in loadedIngredients) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
+  const addIngredientHandler = (ingredient) => {
+    fetch(
+      "https://react-http-55f5b-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) =>
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ])
+      );
+  };
+
+  const onRemoveitem = (id) => {
+    setUserIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredient) => ingredient.id !== id)
+    );
+  };
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddItem={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={onRemoveitem}
+        />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
