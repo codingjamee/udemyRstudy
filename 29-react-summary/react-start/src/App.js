d54fc49ce94b6718bdacf0cfaade5847caef1628
jamee <codingjamee@gmail.com>
2022-12-09 15:17:17 +0900

    react todo리스트

--- 29-react-summary/react-start/src/App.js
+++ 29-react-summary/react-start/src/App.js
@@ -1,4 +1,4 @@
-import Todo from "./Todo";
+import Todo from "./components/Todo";
 
 function App() {
   return (
---
+++ 29-react-summary/react-start/src/components/Backdrop.js
@@ -0,0 +1,4 @@
+const Backdrop = (props) => {
+  return <div className="backdrop" onClick={props.onClose} />;
+};
+export default Backdrop;
